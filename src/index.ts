b4f3fc3363d8035aeeefd0c8486e9b04b50d6def
Devin Riegle <riegledevin@gmail.com>
2024-04-09 16:15:29 -0600

    works

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import chalk from "chalk";
 import { Table } from "console-table-printer";
-import { stat, readdir } from "fs/promises";
+import { stat, readdir, readFile } from "fs/promises";
 import { join } from "path";
 import { program } from "commander";
 import { version } from "../package.json";
 import { Client as LibSQLClient, createClient } from "@libsql/client";
+import { prompt } from "enquirer";
 import { Result } from "./result";
 
 const MIGRATIONS_TABLE = "__turso_migrations";
@@ -19,6 +20,31 @@ type Migration = {
   status: MigrationStatus;
   error?: string;
 };
+
+async function createPendingMigration({
+  client,
+  migrationName,
+}: {
+  client: LibSQLClient;
+  migrationName: string;
+}) {
+  if (!(await hasMigrationsTable(client))) {
+    await client.executeMultiple(`
+      CREATE TABLE ${MIGRATIONS_TABLE} (
+        name TEXT NOT NULL,
+        status TEXT NOT NULL,
+        error TEXT,
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+
+      CREATE UNIQUE INDEX "${MIGRATIONS_TABLE}_name_unique" ON "${MIGRATIONS_TABLE}" ("name");
+    `);
+  }
+
+  await client.execute(`
+    INSERT INTO ${MIGRATIONS_TABLE} (name, status) VALUES ('${migrationName}', 'pending');
+  `);
+}
 
 async function hasMigrationsTable(client: LibSQLClient): Promise<boolean> {
   const result = await client.execute(
@@ -169,7 +195,157 @@ program
       return;
     }
 
-    console.log(chalk.green("Migrating..."));
+    const erroredMigrations = migrationsResult.value.filter(
+      (migration) => migration.status === "errored"
+    );
+
+    if (erroredMigrations.length) {
+      console.log(
+        chalk.red(
+          `Some migrations have errored and need to be fixed first:\n\n${erroredMigrations
+            .map((m) => m.name)
+            .join("\n")}`
+        )
+      );
+      return;
+    }
+
+    const firstPendingMigration = migrationsResult.value.find(
+      (m) => m.status === "pending"
+    );
+
+    if (!firstPendingMigration) {
+      console.log(chalk.green("All migrations have been ran. Nothing to do."));
+      return;
+    }
+
+    const result = await prompt<{ continue: boolean }>([
+      {
+        type: "confirm",
+        name: "continue",
+        message: `Migrate ${firstPendingMigration.name}?`,
+      },
+    ]);
+
+    if (!result.continue) {
+      return;
+    }
+
+    const contents = await readFile(
+      join(firstPendingMigration.path, "up.sql"),
+      "utf-8"
+    );
+
+    await createPendingMigration({
+      client,
+      migrationName: firstPendingMigration.name,
+    });
+
+    try {
+      await client.executeMultiple(contents);
+
+      await client.execute(
+        `UPDATE ${MIGRATIONS_TABLE} SET status = 'completed' WHERE name = '${firstPendingMigration.name}'`
+      );
+
+      console.log(
+        chalk.green(`Migration ${firstPendingMigration.name} succeeded! 🎉`)
+      );
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : `${error}`;
+
+      await client.execute(
+        `UPDATE ${MIGRATIONS_TABLE} SET status = 'errored', error = '${errorMessage}' WHERE name = '${firstPendingMigration.name}'`
+      );
+
+      console.log(
+        chalk.red(
+          `Migration ${firstPendingMigration.name} failed: ${errorMessage}.`
+        )
+      );
+      return;
+    }
+  });
+
+program
+  .command("retry")
+  .description("Retry last errored migration")
+  .requiredOption("-d, --databaseUrl <databaseUrl>", "Turso Database URL")
+  .requiredOption(
+    "-a, --databaseAuthToken <authToken>",
+    "Turso Database Auth Token"
+  )
+  .requiredOption("-s, --schemaFolder <schema>", "Path to schema to migrate")
+  .action(async (options) => {
+    const clientResult = await getClient(options);
+
+    if (!clientResult.success) {
+      console.log(chalk.red(clientResult.error));
+      return;
+    }
+
+    const client = clientResult.value;
+
+    const migrationsResult = await getMigrations({
+      client,
+      schemaFolder: options.schemaFolder,
+    });
+
+    if (!migrationsResult.success) {
+      console.log(chalk.red(migrationsResult.error));
+      return;
+    }
+
+    const lastErroredMigration = migrationsResult.value.find(
+      (migration) => migration.status === "errored"
+    );
+
+    if (!lastErroredMigration) {
+      console.log(chalk.green("There are no errored migrations currently."));
+      return;
+    }
+
+    const result = await prompt<{ continue: boolean }>([
+      {
+        type: "confirm",
+        name: "continue",
+        message: `Migrate ${lastErroredMigration.name}?`,
+      },
+    ]);
+
+    if (!result.continue) {
+      return;
+    }
+
+    const contents = await readFile(
+      join(lastErroredMigration.path, "up.sql"),
+      "utf-8"
+    );
+
+    try {
+      await client.executeMultiple(contents);
+
+      await client.execute(
+        `UPDATE ${MIGRATIONS_TABLE} SET status = 'completed', error = NULL WHERE name = '${lastErroredMigration.name}'`
+      );
+
+      console.log(
+        chalk.green(`Migration ${lastErroredMigration.name} succeeded! 🎉`)
+      );
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : `${error}`;
+
+      await client.execute(
+        `UPDATE ${MIGRATIONS_TABLE} SET status = 'errored', error = '${errorMessage}' WHERE name = '${lastErroredMigration.name}'`
+      );
+
+      console.log(
+        chalk.red(
+          `Migration ${lastErroredMigration.name} failed: ${errorMessage}.`
+        )
+      );
+      return;
+    }
   });
 
 program
@@ -205,8 +381,11 @@ program
       columns: [
         { name: "name", alignment: "center", title: "Name" },
         { name: "status", alignment: "center", title: "Status" },
+        { name: "error", alignment: "center", title: "Error" },
       ],
-      enabledColumns: ["name", "status"],
+      enabledColumns: migrationsResult.value.find((m) => m.status === "errored")
+        ? ["name", "status", "error"]
+        : ["name", "status"],
       rows: migrationsResult.value.map((migration) => ({
         ...migration,
         status:
