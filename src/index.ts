b10c2d1269c66782a19cd6ef021a7df695a42eaa
Devin Riegle <riegledevin@gmail.com>
2024-04-09 13:21:58 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import { program } from "commander";
 import { prompt } from "enquirer";
 import { version } from "../package.json";
 
+// support multiple schemas but not necessarily immediately
+
 program
   .name("turso-migrate")
   .description("CLI tool to make migrations in Turso easier.")
-  .version(version);
-
-program
   .version(version)
   .command("status")
   .description("Shows the status of migrations")
-  .action(() => {});
+  .action(() => {
+    console.log(chalk.green("Hello world"));
+  });
+
+program.parse(process.argv);
